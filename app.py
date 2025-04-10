4f12de74edcf49bb204db46da803503d38162ccb
Nachorc90 <nachorodcar@gmail.com>
2025-04-10 10:51:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -37,13 +37,10 @@ Tras esta prueba empezaremos con los 10 ensayos en las que tienes que responder 
 
 Y para terminar realizaras otros 10 ensayos pero esta vez, tendras que responder con el antonimo a la definición. 
 
-Tener en cuenta: 
-
-En cuanto le de al boton de comenzar el experimento, comenzará. 
-
-Entre ensayos tiene que volver a presionar a continuar para seguir respondiendo.
-
-Cuando haya que cambiar de condición aparecera un mensaje de aviso junto al una botón de continuar. 
+Tener en cuenta:
+- En cuanto le de al boton de comenzar el experimento, comenzará. 
+- Entre ensayos tiene que volver a presionar a continuar para seguir respondiendo.
+- Cuando haya que cambiar de condición aparecera un mensaje de aviso junto al una botón de continuar. 
 """)
 
 # -------- DICCIONARIO DE PALABRAS --------
